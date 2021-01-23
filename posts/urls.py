12af76ea4773bfacc668a4839b469e6e1919f6c2
kanpeipei <satosjhkkmp@gmail.com>
2021-01-23 14:47:03 +0900

    add:add post

--- posts/urls.py
+++ posts/urls.py
@@ -2,9 +2,10 @@ from rest_framework import routers
 from django.conf.urls import url
 from django.urls import path
 
-from .views import GetPostAPI, GetPostsAPI
+from .views import GetPostAPI, PostsAPI, AddPostAPI
 
 urlpatterns = [
-    url(r'', GetPostsAPI.as_view()),
+    url(r'create/', AddPostAPI.as_view()),
+    url(r'', PostsAPI.as_view()),
     path('<int:pk>/', GetPostAPI.as_view()),
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,18 +3,19 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
-from .models import Posts, Tasks
+from .models import Post, Task
 from .serializers import PostSerializer, TaskSerializer
+from accounts.models import Accounts
 
 
 # Create your views here.
-class GetPostsAPI(APIView):
+class PostsAPI(APIView):
   permissions_classes = (permissions.AllowAny,)
   serializer_class = PostSerializer
 
   @transaction.atomic
   def get(self, request, format=None):
-    posts = Posts.objects.all()
+    posts = Post.objects.all().prefetch_related("tasks")
     serializer = self.serializer_class(posts, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -25,8 +26,8 @@ class GetPostAPI(APIView):
   @transaction.atomic
   def get_object(self, pk):
     try:
-      return Posts.objects.get(pk=pk)
-    except Posts.DoesNotExist:
+      return Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
       raise Http404
 
   @transaction.atomic
@@ -34,3 +35,17 @@ class GetPostAPI(APIView):
     post = self.get_object(pk)
     serializer = self.serializer_class(post)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+class AddPostAPI(APIView):
+  permissions_classes = (permissions.AllowAny,)
+  serializer_class = PostSerializer
+
+  @transaction.atomic
+  def post(self, request, format=None):
+    serializer = self.serializer_class(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+      print(serializer.errors)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
