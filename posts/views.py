0eff2a6b4e7656e7d21b9197a6f44d0be20f83a2
kanpeipei <satosjhkkmp@gmail.com>
2021-01-24 01:10:07 +0900

    fix:permission

--- posts/views.py
+++ posts/views.py
@@ -10,17 +10,14 @@ from accounts.models import Accounts
 
 # Create your views here.
 class PostsAPI(APIView):
-  permissions_classes = (permissions.AllowAny,)
   serializer_class = PostSerializer
 
-  @transaction.atomic
   def get(self, request, format=None):
     posts = Post.objects.all().prefetch_related("tasks")
     serializer = self.serializer_class(posts, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 class GetPostAPI(APIView):
-  permissions_classes = (permissions.AllowAny,)
   serializer_class = PostSerializer
 
   @transaction.atomic
@@ -30,17 +27,14 @@ class GetPostAPI(APIView):
     except Post.DoesNotExist:
       raise Http404
 
-  @transaction.atomic
   def get(self, request, pk, format=None):
     post = self.get_object(pk)
     serializer = self.serializer_class(post)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 class AddPostAPI(APIView):
-  permissions_classes = (permissions.AllowAny,)
   serializer_class = PostSerializer
 
-  @transaction.atomic
   def post(self, request, format=None):
     serializer = self.serializer_class(data=request.data)
     if serializer.is_valid():
--- python_api_2/settings.py
+++ python_api_2/settings.py
@@ -133,8 +133,10 @@ JWT_AUTH = {
 
 REST_FRAMEWORK = { 
     'DEFAULT_PERMISSION_CLASSES': (
+        # tokenをヘッダーに載せないとUnauthorizedにする
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
+        # 誰でもAPIを叩けるようにする
+        # 'rest_framework.permissions.AllowAny',
     ),  
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
