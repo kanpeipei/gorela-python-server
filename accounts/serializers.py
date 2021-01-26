b78d5224407269822829e63a19c2652c6e564c3b
kanpeipei <satosjhkkmp@gmail.com>
2021-01-27 03:52:34 +0900

    add:follow function

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Accounts, AccountsManager
+from .models import Accounts, AccountsManager, Relation
 
 
 class AccountsSerializer(serializers.ModelSerializer):
@@ -12,3 +12,34 @@ class AccountsSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Accounts.objects.create_user(validated_data['username'],validated_data['password'])
+
+class FollowAccountSerializer(serializers.ModelSerializer):
+    # follow_user = AccountsSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(queryset=Accounts.objects.all(), write_only=True)
+    target_user_id = serializers.PrimaryKeyRelatedField(queryset=Accounts.objects.all(), write_only=True)
+
+    class Meta:
+        model = Relation
+        fields = ('id', 'user', 'user_id', 'target_user_id')
+
+    def create(self, validated_data):
+        validated_data['user'] = validated_data.get('user_id', None)
+        if validated_data['user'] is None:
+            raise serializers.ValidationError("user not found.")
+        del validated_data['user_id']
+
+        validated_data['target_user'] = validated_data.get('target_user_id', None)
+        if validated_data['target_user'] is None:
+            raise serializers.ValidationError("target user not found.")
+        del validated_data['target_user_id']
+
+        relation = Relation.objects.create(**validated_data)
+        return relation
+
+class FollowerAccountSerializer(serializers.ModelSerializer):
+    # followed_user = AccountsSerializer(read_only=True)
+
+    class Meta:
+        model = Relation
+        fields = ('id', 'target_user')
+
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,8 @@ from django.db import transaction
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
-from .models import Accounts
-from .serializers import AccountsSerializer
+from .models import Accounts, Relation
+from .serializers import AccountsSerializer, FollowAccountSerializer
 from posts.serializers import AccountDetailSerializer
 
 
@@ -44,3 +44,30 @@ class UpdateAccountAPI(APIView):
       serializer.save()
       return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class RelationAPI(APIView):
+  @transaction.atomic
+  def get_object(self, pk):
+    try:
+      return Accounts.objects.get(pk=pk)
+    except Accounts.DoesNotExist:
+      raise Http404
+
+  def post(self, request, user_id, target_user_id):
+    request.data['user_id'] = user_id
+    request.data['target_user_id'] = target_user_id
+    serializer = FollowAccountSerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+      print(serializer.errors)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, user_id, target_user_id):
+    user = self.get_object(user_id)
+    target_user = self.get_object(target_user_id)
+    relation = Relation.objects.filter(user=user, target_user=target_user)
+    relation.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
